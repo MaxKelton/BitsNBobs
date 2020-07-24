cd81cb56648426368fe08496a4dd96be5eb4e45a
Max Kelton <max@amateurgeek.com>
2020-07-24 00:47:10 -0700

    Added KeyboardHelper for events, and the first of many wool stairs

--- src/main/java/com/greenrazor/bitsnbobs/util/RegistryHandler.java
+++ src/main/java/com/greenrazor/bitsnbobs/util/RegistryHandler.java
@@ -6,6 +6,10 @@ import com.greenrazor.bitsnbobs.items.NiceCrispyBacon;
 import com.greenrazor.bitsnbobs.items.PoisonApple;
 import com.greenrazor.bitsnbobs.items.RawBacon;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -32,7 +36,13 @@ public class RegistryHandler {
 
     // Blocks
     public static final RegistryObject<Block> WITCH_OVEN = BLOCKS.register("witch_oven", WitchOven::new);
+    public static final RegistryObject<Block> WOOL_STAIRS = BLOCKS.register("wool_stairs",
+            () -> new StairsBlock(() -> Blocks.WHITE_WOOL.getDefaultState(),
+                    Block.Properties.create(Material.WOOL).sound(SoundType.CLOTH)));
 
     // Block Items
-    public static final RegistryObject<Item> WITCH_OVEN_ITEM = ITEMS.register("witch_oven", () -> new BlockItem(WITCH_OVEN.get(), new Item.Properties().group(BitsNBobs.TAB)));
+    public static final RegistryObject<Item> WITCH_OVEN_ITEM = ITEMS.register("witch_oven",
+            () -> new BlockItem(WITCH_OVEN.get(), new Item.Properties().group(BitsNBobs.TAB)));
+    public static final RegistryObject<Item> WOOL_STAIRS_ITEM = ITEMS.register("wool_stairs",
+            () -> new BlockItem(WOOL_STAIRS.get(), new Item.Properties().group(BitsNBobs.TAB)));
 }
